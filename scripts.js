586deba9528e27eaa5e236a27f20a51b4188b5a6
FAGNERBCARVALHO <fa.carvalho117@gmail.com>
2024-07-15 20:16:40 -0300

    new repository

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let formulario = document.getElementById('contato')
+const mascara = document.querySelector(".mascara-formulario")
+
+function aparecerFormulario(){
+    formulario.style.left = "50%"
+    formulario.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+
+}
+
+function desaparecerFormulario(){
+    formulario.style.left = "-300px"
+    formulario.style.transform = "translateX(0)"
+    mascara.style.visibility = "hidden"
+
+}
+console.log(aparecerFormulario)
